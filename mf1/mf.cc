4208d8df9950b9715370b8ff4fa2554ca7cdbe34
Nestor Castillo Garcia <nestorcgk@gmail.com>
2015-04-15 19:14:41 +0300

    Working median filter

--- mf1/mf.cc
+++ mf1/mf.cc
@@ -1,20 +1,27 @@
 #include "mf.h"
-#include <iostream>
-#include <queue>
-using namespace std;
+
+
+
+
+
+void clear(vector<float> &q)
+{
+    vector<float>  empty;
+    swap( q, empty );
+}
 
 void mf(int ny, int nx, int hy, int hx, const float* in, float* out)
 {
     // FIXME
 	int edgex = hx/2;
 	int edgey = hy/2;
-	int limit = (ny*nx) - (ny*nx);
 	int xwind = 0;
 	int ywind = 0;
+	vector<float> window;
     
-    for (int y = edgey; i < ny-edgey; i++)
+    for (int y = edgey; y < ny-edgey; y++)
     {
-    	for (int x = edgex; y < nx-edgex; y++)
+    	for (int x = edgex; x < nx-edgex; x++)
     	{
     		for (int wx = 0; wx < hx; wx++)
     		{
@@ -22,38 +29,18 @@ void mf(int ny, int nx, int hy, int hx, const float* in, float* out)
     			{
     				xwind = x + wx - edgex;
     				ywind = y + wy - edgey;
-    				window.push(in[xwind + nx*ywind]);
+    				window.push_back(in[xwind + nx*ywind]);
     			}
     		}
-    		out[x + nx*y] = median(window);//median 
+    		nth_element(window.begin(), window.begin() + window.size()/2, window.end());
+    		out[x + nx*y] = window[window.size()/2];//median 
+    		clear(window);
     	}
     }
 
-void clear(priority_queue<float, std::vector<float>, std::greater<float>> &q)
-{
-    priority_queue<float, std::vector<float>, std::greater<float> >  empty;
-    swap( q, empty );
-}
 
-float median(priority_queue<float, std::vector<float>, std::greater<float>> &q){
-    float res = 0.0;
-    float aux = 0.0;
-    long initSize = q.size();
-    while (q.size() > initSize/2 +1) {
-        q.pop();
-    }
-    if (initSize % 2 == 0) {
-        aux = q.top();
-        q.pop();
-        res = (aux + q.top())/2.0;
-    }else{
-        //q.pop();
-        res = q.top();
-    }
-    clear(q);
-    
-    return res;
-}
+
+
 
     //for (int i = edgex; i < ny * nx; ++i) {
     //    out[i] = in[i];
