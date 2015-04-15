d4ed91a910f3281e3d23be511bb6fee49478d97a
Nestor Castillo Garcia <nestorcgk@gmail.com>
2015-04-15 20:17:23 +0300

    Added multi threaded implementation

--- mf2/mf.cc
+++ mf2/mf.cc
@@ -1,8 +1,47 @@
 #include "mf.h"
 
-void mf(int ny, int nx, int hy, int hx, const float* in, float* out) {
-    // FIXME
-    for (int i = 0; i < ny * nx; ++i) {
-        out[i] = in[i];
+
+
+
+
+void clear(vector<float> &q)
+{
+    vector<float>  empty;
+    swap( q, empty );
+}
+
+void mf(int ny, int nx, int hy, int hx, const float* in, float* out)
+{
+	int edgex = hx/2;
+	int edgey = hy/2;
+	int xwind = 0;
+	int ywind = 0;
+	vector<float> window;
+    #pragma omp parallel for   
+    for (int y = edgey; y < ny-edgey; y++)
+    {
+    	for (int x = edgex; x < nx-edgex; x++)
+    	{
+    		for (int wx = 0; wx < hx; wx++)
+    		{
+    			for(int wy = 0; wy <hy ; wy++)
+    			{
+    				xwind = x + wx - edgex;
+    				ywind = y + wy - edgey;
+    				window.push_back(in[xwind + nx*ywind]);
+    			}
+    		}
+    		nth_element(window.begin(), window.begin() + window.size()/2, window.end());
+    		out[x + nx*y] = window[window.size()/2];//median 
+    		clear(window);
+    	}
     }
+
+
+
+
+
+    //for (int i = edgex; i < ny * nx; ++i) {
+    //    out[i] = in[i];
+    //}
 }
