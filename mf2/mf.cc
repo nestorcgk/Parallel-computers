a3f69ce369f8bbf81996ccd43c1753a95239471c
Nestor Castillo Garcia <nestorcgk@gmail.com>
2015-04-17 18:36:04 +0300

    Change vector to array

--- mf2/mf.cc
+++ mf2/mf.cc
@@ -1,19 +1,21 @@
 #include "mf.h"
 
-float median(vector<float> &med)
+float median(float * med,int k)
 {
     float median = 0.0;
-    nth_element(med.begin(), med.begin() + med.size()/2, med.end());
+    //nth_element(med.begin(), med.begin() + med.size()/2, med.end());
+    nth_element(med + 0, med + k/2, med +k);
 
-    if(med.size() % 2 == 0)
+    if(k % 2 == 0)
     {
-        median = med[med.size()/2];
-        nth_element(med.begin(), med.begin() + med.size()/2 -1, med.end());
-        median = (median + med[(med.size()/2) -1])/2.0;
+        median = med[k/2];
+        //nth_element(med.begin(), med.begin() + med.size()/2 -1, med.end());
+        nth_element(med + 0, med + k/2 -1, med +k);
+        median = (median + med[(k/2) -1])/2.0;
 
     }else
     {
-        median = med[med.size()/2];
+        median = med[k/2];
     }
     return median;
 }
@@ -30,13 +32,14 @@ void mf(int ny, int nx, int hy, int hx, const float* in, float* out)
     #pragma omp parallel for  
     for (int y = 0; y < ny; y++)
     {
-        vector<float> window;
-        window.reserve(hx*nhy);
-        
+        //vector<float> window;
+        //window.reserve(hx*nhy);
+        float *window = new float[nhy*nhx];
+
         for (int x = 0; x < nx; x++)
         {
-            
-            window.clear();
+            int k = 0;
+            //window.clear();
             for (int wx = 0; wx < nhx; wx++)
             {
                 for(int wy = 0; wy <nhy ; wy++)
@@ -45,12 +48,15 @@ void mf(int ny, int nx, int hy, int hx, const float* in, float* out)
                     ywind = y + wy - edgey;
                     if(xwind >= 0 && xwind <nx && ywind >= 0 && ywind < ny)
                     {
-                        window.push_back(in[xwind + nx*ywind]);
+                        //window.push_back(in[xwind + nx*ywind]);
+                        //window.insert(window.begin()+k,in[xwind + nx*ywind]);
+                        window[k] = in[xwind + nx*ywind];
+                        k++;
                     }
                     
                 }
             }
-            out[x + nx*y] = median(window);
+            out[x + nx*y] = median(window,k);
         }
     }
 }
