d46cb7f34cd1a2dbed3ed5b4e7b3b8b15f8cc3f9
Nestor Castillo Garcia <nestorcgk@gmail.com>
2015-05-02 18:52:00 +0300

    Sorting algorithm working, good performance first case

--- so1/so.cc
+++ so1/so.cc
@@ -1,7 +1,38 @@
 #include "so.h"
 #include <algorithm>
+#include <omp.h>
+using namespace std;
+
 
 void psort(int n, data_t* data) {
-    // FIXME: make this more efficient with parallelism
-    std::sort(data, data + n);
+    
+    int numBlocks = omp_get_max_threads();
+    int blockSize = n/numBlocks;
+    
+    #pragma omp parallel num_threads(numBlocks)
+    {
+    #pragma omp for
+    for (int i = 0; i < numBlocks; ++i)
+    {
+        sort(data + i * blockSize,data + (i+1)*blockSize);
+    }
+	}
+    
+    while (numBlocks/2 >= 1){
+        #pragma omp parallel num_threads(numBlocks/2)
+        {
+        #pragma omp for
+        for (int j = 0; j < numBlocks/2; j ++) {
+            data_t *temp = new data_t[blockSize*2];            
+            int first = 2 * j * blockSize;
+            int last = first + blockSize;
+            int lastIndex = min(last + blockSize,n);
+            merge(data + first,data + first + blockSize,data + last,data + lastIndex,temp);
+            copy(temp,temp+(blockSize*2),data + first);
+        }
+       	} 
+        numBlocks /= 2;
+        blockSize *= 2;
+    }
 }
+
