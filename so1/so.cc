91ad870b41ee09389822fbc679267722c83fae72
Nestor Castillo Garcia <nestorcgk@gmail.com>
2015-05-02 22:06:43 +0300

    Improved efficiency with inline merge

--- so1/so.cc
+++ so1/so.cc
@@ -9,28 +9,29 @@ void psort(int n, data_t* data) {
     int numBlocks = omp_get_max_threads();
     int blockSize = n/numBlocks;
     
-    //#pragma omp parallel num_threads(numBlocks)
-    //{
-    #pragma omp parallel for
+    #pragma omp parallel for 
     for (int i = 0; i < numBlocks; ++i)
     {
-        sort(data + i * blockSize,data + (i+1)*blockSize);
+        if(i == numBlocks -1){
+            sort(data + i * blockSize,data + n);
+        }else{
+            sort(data + i * blockSize,data + (i + 1) * blockSize);
+        }
     }
 
-    
     while (numBlocks/2 >= 1){
-        //#pragma omp parallel num_threads(numBlocks/2)
-        //{
-        #pragma omp parallel for
-        for (int j = 0; j < numBlocks/2; j ++) {
-            data_t *temp = new data_t[blockSize*2];            
+        #pragma omp parallel for 
+        for (int j = 0; j < numBlocks / 2; j ++) {           
             int first = 2 * j * blockSize;
             int last = first + blockSize;
-            int lastIndex = min(last + blockSize,n);
-            merge(data + first,data + first + blockSize,data + last,data + lastIndex,temp);
-            copy(temp,temp+(blockSize*2),data + first);
+            int lastIndex = last + blockSize;
+            if(j == numBlocks/2 -1)
+            {
+                inplace_merge(data + first,data + last,data + n);
+            }else{
+                inplace_merge(data + first,data + last,data + lastIndex);
+            }
         }
-       	//} 
         numBlocks /= 2;
         blockSize *= 2;
     }
