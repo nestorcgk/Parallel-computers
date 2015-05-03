9baed2f8ec8bebf9dd253bbda29bfe4a7dc454d9
Nestor Castillo Garcia <nestorcgk@gmail.com>
2015-05-03 09:25:26 +0300

    Add check numThreads is a power of two if not, sorts without partition.

--- so1/so.cc
+++ so1/so.cc
@@ -7,17 +7,29 @@ using namespace std;
 void psort(int n, data_t* data) {
     
     int numBlocks = omp_get_max_threads();
+    //int numThreads = 0;
     int blockSize = n/numBlocks;
+    bool adeqThreads = 1;
     
-    #pragma omp parallel for 
-    for (int i = 0; i < numBlocks; ++i)
-    {
-        if(i == numBlocks -1){
-            sort(data + i * blockSize,data + n);
-        }else{
-            sort(data + i * blockSize,data + (i + 1) * blockSize);
+    //checks that the threads are a power of two
+    
+        #pragma omp parallel for 
+        for (int i = 0; i < numBlocks; ++i)
+        {
+            if(i == numBlocks -1){
+                sort(data + i * blockSize,data + n);
+            }else{
+                sort(data + i * blockSize,data + (i + 1) * blockSize);
+            }
+            int numThreads = omp_get_num_threads();
+            //checks if the number of threads is a power of two
+            if(omp_get_thread_num() == 0){
+            adeqThreads = !(numThreads == 0) && !(numThreads & (numThreads - 1));
+            }
         }
-    }
+     
+    if (adeqThreads)
+    {
 
     while (numBlocks/2 >= 1){
         #pragma omp parallel for 
@@ -28,12 +40,16 @@ void psort(int n, data_t* data) {
             if(j == numBlocks/2 -1)
             {
                 inplace_merge(data + first,data + last,data + n);
-            }else{
+                }else{
                 inplace_merge(data + first,data + last,data + lastIndex);
+                }
             }
+            numBlocks /= 2;
+            blockSize *= 2;
         }
-        numBlocks /= 2;
-        blockSize *= 2;
+    }else
+    {
+        sort(data + 0,data + n);
     }
 }
 
