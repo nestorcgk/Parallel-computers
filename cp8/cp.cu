009c80ce097dcbf3b590ed14ba2c8f2a48e82dc4
Nestor Castillo Garcia <nestorcgk@gmail.com>
2015-05-06 14:22:41 +0300

    Update

--- cp8/cp.cu
+++ cp8/cp.cu
@@ -1,6 +1,7 @@
 #include "cp.h"
 #include <cuda_runtime.h>
 #include <stdio.h>
+#include <math.h>
 
 
 __global__ void cube(float * d_out, float * d_in){
@@ -9,44 +10,90 @@ __global__ void cube(float * d_out, float * d_in){
     d_out[idx] = f*f*f;
 }
 
+void normaliseInput(int ny, int nx, double* normalised, const float* data){
+    //#pragma omp parallel for
+    for (int rowj = 0; rowj < ny; rowj++)
+    {
+        double sumSqRow = 0.0;
+        double mean = 0.0;
+        //substract mean
+        for (int i = rowj*nx; i < rowj*nx + nx; i++)
+        {
+            mean += (double)data[i];
+        }
+        mean /= (double)nx;
+    
+        for (int i = rowj*nx; i < rowj*nx + nx; i++)
+        {
+            double value = (double) data[i] - mean;
+            normalised[i] = value;
+            sumSqRow += pow(value,2);
+        }
+        double value2 = sqrt(sumSqRow);
+        for (int i = rowj*nx; i < rowj*nx + nx; i++)
+        {
+            normalised[i] /= value2;
+        }
+    }
+}
+//calculates correlation of two rows given a normalised matrix
+__global__ void matProduct(int ny, int nx,int vec1,int vec2, double* normalised){
+    double res = 0.0;
+    //matrix[x + y*nx]
+    
+    for (int i = 0; i < nx; i++) {
+        res += normalised[i + vec1*nx]*normalised[i + vec2*nx];
+    }
+    result[vec1+vec2*ny] = res;
+}
+
+
+
 void correlate(int ny, int nx, const float* data, float* result) {
     
     //for (int i = 0; i < ny * ny; ++i) {
     //    result[i] = 0.0f;
     //}
-    const int ARRAY_SIZE = 96;
+    const int ARRAY_SIZE = ny*nx;
 	const int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
 
-	// generate the input array on the host
-	float h_in[ARRAY_SIZE];
-	for (int i = 0; i < ARRAY_SIZE; i++) {
-		h_in[i] = float(i);
-	}
-	float h_out[ARRAY_SIZE];
-
 	// declare GPU memory pointers
-	float * d_in;
-	float * d_out;
+	float * d_data;
+	float * d_result;
+
+	//Normalize input
+	double *normalised = new double[ny*nx];
+    normaliseInput(ny,nx,normalised,data);
 
 	// allocate GPU memory
-	cudaMalloc((void**) &d_in, ARRAY_BYTES);
-	cudaMalloc((void**) &d_out, ARRAY_BYTES);
+	cudaMalloc((void**) &d_data, ARRAY_BYTES);
+	cudaMalloc((void**) &d_result, ARRAY_BYTES);
 
 	// transfer the array to the GPU
-	cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_data, data, ARRAY_BYTES, cudaMemcpyHostToDevice);
 
 	// launch the kernel
-	cube<<<1, ARRAY_SIZE>>>(d_out, d_in);
+	const dim3 blockSize(128, 1, 1);  
+  	const dim3 gridSize(ceil(nx/128.0), 1, 1);
+
+
+
+
+    #pragma omp parallel for
+    for (int i = 0; i < ny; i++)//(int i = 0; i < ny; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+        	matProduct<<<gridSize, blockSize>>>(d_result, d_data);
+            //result[i+j*ny] = matProduct(ny, nx, i, j, normalised);
+
+        }
+    }
+	
 
 	// copy back the result array to the CPU
-	cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+	cudaMemcpy(result, d_result, ARRAY_BYTES, cudaMemcpyDeviceToHost);
 
-	// print out the resulting array
-	for (int i =0; i < ARRAY_SIZE; i++) {
-		printf("%f", h_out[i]);
-		printf(((i % 4) != 3) ? "\t" : "\n");
-	}
-
-	cudaFree(d_in);
-	cudaFree(d_out);
+	cudaFree(d_data);
+	cudaFree(d_result);
 }
