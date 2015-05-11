05a26e5990361ca2a8951f627adb7041c761e92a
Jukka Suomela <jukka.suomela@iki.fi>
2015-05-11 22:42:50 +0300

    CP: benchmark output to files supported.

--- cp0/cp-benchmark.cc
+++ cp0/cp-benchmark.cc
@@ -1,3 +1,5 @@
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <vector>
 #include <random>
@@ -5,7 +7,7 @@
 #include "timer.h"
 #include "cp.h"
 
-static void benchmark(int ny, int nx) {
+static void benchmark(std::ostream& f, int ny, int nx) {
     std::mt19937 rng;
     std::uniform_real_distribution<float> u(0.0f, 1.0f);
     std::vector<float> data(ny * nx);
@@ -16,9 +18,9 @@ static void benchmark(int ny, int nx) {
             data[x + nx * y] = v;
         }
     }
-    std::cout << "cp\t" << ny << "\t" << nx << "\t" << std::flush;
-    { Timer t; correlate(ny, nx, data.data(), result.data()); }
-    std::cout << std::endl;
+    f << "cp\t" << ny << "\t" << nx << "\t" << std::flush;
+    { Timer t(false, f); correlate(ny, nx, data.data(), result.data()); }
+    f << std::endl;
 }
 
 int main(int argc, const char** argv) {
@@ -27,5 +29,19 @@ int main(int argc, const char** argv) {
     }
     int ny = std::stoi(argv[1]);
     int nx = std::stoi(argv[2]);
-    benchmark(ny, nx);
+
+    const char* outfile = std::getenv("PPC_OUTPUT");
+    if (outfile) {
+        std::ofstream f(outfile);
+        if (f.fail()) {
+            error(outfile, "cannot open for writing");
+        }
+        benchmark(f, ny, nx);
+        f.close();
+        if (f.fail()) {
+            error(outfile, "write error");
+        }
+    } else {
+        benchmark(std::cout, ny, nx);
+    }
 }
